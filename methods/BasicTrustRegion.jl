dc0c91488605e44e19d46fdf2a8d8464bb636d7f
brilhana <alexandre.brilhante@gmail.com>
2017-09-21 19:07:45 -0400

    Update.

--- methods/BasicTrustRegion.jl
+++ methods/BasicTrustRegion.jl
@@ -1,3 +1,4 @@
+using ForwardDiff
 using Optim
 
 immutable BasicTrustRegion{T <: Real}
@@ -13,8 +14,8 @@ end
 
 type BTRState
     iter::Int
-    x::Vector
-    xcand::Vector
+    β::Vector
+    βcand::Vector
     g::Vector
     step::Vector
     Δ::Float64
@@ -28,18 +29,17 @@ end
 
 function acceptCandidate!(state::BTRState, b::BasicTrustRegion)
     # If the iteration is successful, update the iterate
-    if (state.ρ >= b.η1)
-        return true
-    else
-        return false
+    if state.ρ >= b.η1
+        true
     end
+    false
 end
 
 function updateRadius!(state::BTRState, b::BasicTrustRegion)
-    if (state.ρ >= b.η2)
+    if state.ρ >= b.η2
         stepnorm = norm(state.step)
         state.Δ = min(1e20, max(4*stepnorm, state.Δ))
-    elseif (state.ρ >= b.η1)
+    elseif state.ρ >= b.η1
         state.Δ *= b.γ2
     else
         state.Δ *= b.γ1
@@ -49,86 +49,65 @@ end
 function CauchyStep(g::Vector, H::Matrix, Δ::Float64)
     q = dot(g, H*g)
     normg = norm(g)
-    if (q <= 0)
+    if q <= 0
         τ = 1.0
     else
         τ = min((normg*normg*normg)/(q*Δ), 1.0)
     end
-    return -τ*g*Δ/normg
+    -τ*g*Δ/normg
 end
 
-function btr(f::Function, g!::Function, H!::Function, x0::Vector, tol::Float64 = 1e-8, verbose::Bool = false)
+function btr(f::Function, g!::Function, H!::Function,
+        β0::Vector, tol::Float64 = 1e-8, verbose::Bool = false)
     b = BTRDefaults()
     state = BTRState()
     state.iter = 0
     state.Δ = 1.0
-    state.x = x0
-    n = length(x0)
+    state.β = β0
+    n = length(β0)
     tol2 = tol*tol
     state.g = zeros(n)
     H = zeros(n, n)
-    fx = f(x0)
-    g!(x0, state.g)
-    H!(x0, H)
+    fβ = f(β0)
+    g!(β0, state.g)
+    H!(β0, H)
     nmax = 100000
 
     function model(s::Vector, g::Vector, H::Matrix)
-        return dot(s, g)+0.5*dot(s, H*s)
+        dot(s, g)+0.5*dot(s, H*s)
     end
 
-    while (dot(state.g, state.g) > tol2 && state.iter < nmax)
+    while dot(state.g, state.g) > tol2 && state.iter < nmax
         # Compute the step by approximately minimize the model
         state.step = CauchyStep(state.g, H, state.Δ)
-        state.xcand = state.x+state.step
+        state.βcand = state.β+state.step
         # Compute the actual reduction over the predicted reduction
-        fcand = f(state.xcand)
-        state.ρ = (fcand-fx)/(model(state.step, state.g, H))
-        if (acceptCandidate!(state, b))
-            state.x = copy(state.xcand)
-            g!(state.x, state.g)
-            H!(state.x, H)
-            fx = fcand
+        fcand = f(state.βcand)
+        state.ρ = (fcand-fβ)/(model(state.step, state.g, H))
+        if acceptCandidate!(state, b)
+            state.β = copy(state.βcand)
+            g!(state.β, state.g)
+            H!(state.β, H)
+            fβ = fcand
         end
         updateRadius!(state, b)
         state.iter += 1
     end
-    return state
+    state
 end
 
-function rosenbrock(x::Vector)
-    return (1.0 - x[1])^2 + 100.0 * (x[2] - x[1]^2)^2
-end
+f(β::Vector) = -10*β[1]^2+10*β[2]^2+4*sin(β[1]*β[2])-2*β[1]+β[1]^4
 
-function rosenbrock_gradient!(x::Vector, storage::Vector)
-    storage[1] = -2.0 * (1.0 - x[1]) - 400.0 * (x[2] - x[1]^2) * x[1]
-    storage[2] = 200.0 * (x[2] - x[1]^2)
-end
+g = β -> ForwardDiff.gradient(f, β)
+H = β -> ForwardDiff.hessian(f, β)
 
-function rosenbrock_hessian!(x::Vector, storage::Matrix)
-    storage[1, 1] = 2.0 - 400.0 * x[2] + 1200.0 * x[1]^2
-    storage[1, 2] = -400.0 * x[1]
-    storage[2, 1] = -400.0 * x[1]
-    storage[2, 2] = 200.0
-end
-
-state = btr(rosenbrock, rosenbrock_gradient!, rosenbrock_hessian!, [0,0])
-
-state
-
-using ForwardDiff
-
-f(x::Vector) = -10*x[1]^2+10*x[2]^2+4*sin(x[1]*x[2])-2*x[1]+x[1]^4
-
-g = x -> ForwardDiff.gradient(f, x);
-H = x -> ForwardDiff.hessian(f, x)
-
-function g!(x::Vector, storage::Vector)
-    s = g(x)
+function g!(β::Vector, storage::Vector)
+    s = g(β)
     storage[1:length(s)] = s[1:length(s)]
 end
 
-function H!(x::Vector, storage::Matrix)
-    s = H(x)
+function H!(β::Vector, storage::Matrix)
+    s = H(β)
     n, m = size(s)
     storage[1:n, 1:m] = s[1:length(s)]
 end
--- methods/Newton.jl
+++ methods/Newton.jl
@@ -25,45 +25,45 @@ function f(β::Vector)
     c9 = exp(β[1]*40+β[2]*30)/(exp(β[1]*40+β[2]*30)+exp(β[1]*200+β[2]*150))
     c0 = exp(β[1]*90+β[2]*80)/(exp(β[1]*90+β[2]*80)+exp(β[1]*140+β[2]*130))
     return (log(c1)+log(c2)+log(c3)+log(c4)+log(c5)+log(c6)+log(c7)+log(c8)+
-    log(c9)+log(c0))/length(β)
+        log(c9)+log(c0))/length(β)
 end
 
-g = x -> ForwardDiff.gradient(f, x)
-H = x -> ForwardDiff.hessian(f, x)
+g = β -> ForwardDiff.gradient(f, β)
+H = β -> ForwardDiff.hessian(f, β)
 
-function g!(x::Vector, storage::Vector)
-    s = g(x)
+function g!(β::Vector, storage::Vector)
+    s = g(β)
     storage[1:length(s)] = s[1:length(s)]
 end
 
-function H!(x::Vector, storage::Matrix)
-    s = H(x)
+function H!(β::Vector, storage::Matrix)
+    s = H(β)
     n, m = size(s)
     storage[1:n, 1:m] = s[1:length(s)]
 end
 
 function newton(f::Function, g::Function, h:: Function,
-        xstart::Vector, verbose::Bool = false,
+        β0::Vector, verbose::Bool = false,
         δ::Float64 = 1e-8, nmax::Int64 = 1000)
     k = 1
-    x = xstart
-    n = length(x)
+    β = β0
+    n = length(β)
     δ2 = δ*δ
     H = eye(n)
-    dfx = ones(n)
+    dfβ = ones(n)
     if verbose
-        fx = f(x)
-        println("$k. x = $x, f(x) = $fx")
+        fβ = f(β)
+        println("$k. β = $β, f(β) = $fβ")
     end
-    g(x, dfx)
-    while dot(dfx, dfx) > δ2 && k <= nmax
+    g(β, dfβ)
+    while dot(dfβ, dfβ) > δ2 && k <= nmax
         k += 1
-        g(x, dfx)
-        h(x, H)
-        x -= H\dfx
+        g(β, dfβ)
+        h(β, H)
+        β -= H\dfβ
         if verbose
-            fx = f(x)
-            println("$k. x = $x, f(x) = $fx")
+            fβ = f(β)
+            println("$k. β = $β, f(β) = $fβ")
         end
     end
     x
