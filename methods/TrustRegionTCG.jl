8f03501bbe55799e91d8d89f98b9b47e34ecac34
brilhana <alexandre.brilhante@gmail.com>
2017-10-05 14:10:12 -0400

    Update.

--- methods/TrustRegionTCG.jl
+++ methods/TrustRegionTCG.jl
@@ -22,13 +22,10 @@ type BTRState
     Δ::Float64
     ρ::Float64
     δ::Float64
-    trace
-    keepTrace::Bool
 
     function BTRState()
         state = new()
         state.δ = 1e-8
-        state.keepTrace = false
         return state
     end
 end
@@ -52,59 +49,32 @@ function updateRadius!(state::BTRState, b::BasicTrustRegion)
     end
 end
 
-function btr(f::Function, g!::Function, H!::Function, Step::Function, β0::Vector, state::BTRState = BTRState(), ApproxH::Bool = false, verbose::Bool = false)
+function btr(f::Function, g!::Function, H!::Function, Step::Function, β0::Vector)
+state::BTRState = BTRState()
     b = BTRDefaults()
     state.iter = 0
     state.β = β0
     n = length(β0)
     δ2 = state.δ*state.δ
     state.g = zeros(n)
-    # A better initialization procedure should be used with quasi-Newton approximations
-    # We could rely on some preconditioner.
     H = eye(n, n)
     fβ = f(β0)
     g!(β0, state.g)
     state.Δ = 0.1*norm(state.g)
-    if ApproxH
-        y = zeros(n)
-        gcand = zeros(n)
-        # H!(H, y, state.step)
-    else
-        H!(β0, H)
-    end
     nmax = 1000
-    if state.keepTrace
-        state.trace = β0'
-    end
 
     function model(s::Vector, g::Vector, H::Matrix)
         return dot(s, g)+0.5*dot(s, H*s)
     end
 
     while (dot(state.g, state.g) > δ2 && state.iter < nmax)
-        # Compute the step by approximately minimize the model
         state.step = Step(state.g, H, state.Δ)
         state.βcand = state.β+state.step
-        # Compute the actual reduction over the predicted reduction
         fcand = f(state.βcand)
         state.ρ = (fcand-fβ)/(model(state.step, state.g, H))
-        if ApproxH
-            g!(state.βcand, gcand)
-            y = gcand-state.g;
-            H = H!(H, y, state.step)
-        end
-        if (acceptCandidate!(state, b))
+        if acceptCandidate!(state, b)
             state.β = copy(state.βcand)
-            if ApproxH == false
-                g!(state.β, state.g)
-                H!(state.β, H)
-            else
-                state.g = copy(gcand)
-            end
             fβ = fcand
-        end
-        if state.keepTrace
-            state.trace = [state.trace ; state.β']
         end
         updateRadius!(state, b)
         state.iter += 1
@@ -112,12 +82,9 @@ function btr(f::Function, g!::Function, H!::Function, Step::Function, β0::Vecto
     return state.β, state.iter
 end
 
-function BFGSUpdate(B::Matrix, y::Vector, s::Vector)
-    Bs = B*s
-    return B - (Bs*Bs')/dot(s, Bs) + (y*y')/dot(s, y)
-end
-
-function stopCG(normg::Float64, normg0::Float64, k::Int, kmax::Int, χ::Float64 = 0.1, θ::Float64 = 0.5)
+function stopCG(normg::Float64, normg0::Float64, k::Int, kmax::Int)
+    χ::Float64 = 0.1
+    θ::Float64 = 0.5
     if ((k == kmax) || (normg <= normg0*min(χ, normg0^θ)))
         return true
     else
@@ -140,21 +107,18 @@ function TruncatedCG(g::Vector, H::Matrix, Δ::Float64)
     while stopCG(norm(g), normg0, k, n) == false
         Hd = H*d
         κ = dot(d, Hd)
-        # Is the curvature negative in the direction d?
         if (κ <= 0)
             σ = (-sMd+sqrt(sMd*sMd+norm2d*(Δ2-dot(s, s))))/norm2d
             s += σ*d
             break
         end
         α = gv/κ
-        # Check is the model minimizer is outside the trust region
         norm2s += α*(2*sMd+α*norm2d)
         if norm2s >= Δ2
             σ = (-sMd+sqrt(sMd*sMd+norm2d*(Δ2-dot(s, s))))/norm2d
             s += σ*d
             break
         end
-        # The model minimizer is inside the trust region
         s += α*d
         g += α*Hd
         v = g
